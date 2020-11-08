d92f8b3b7084abb47e09895e3145ac4c34c04abf
Matthew Eskolin <matteskolin@gmail.com>
2020-11-07 19:52:47 -0600

    cleanup

--- Program.cs
+++ Program.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Azure.KeyVault;
@@ -9,7 +5,6 @@ using Microsoft.Azure.Services.AppAuthentication;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Configuration.AzureKeyVault;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace Garmin_To_Fitbit_Steps_Sync_Web
 {
@@ -20,19 +15,11 @@ namespace Garmin_To_Fitbit_Steps_Sync_Web
             CreateWebHostBuilder(args).Build().Run();
         }
 
-        // public static IHostBuilder CreateHostBuilder(string[] args) =>
-        //     Host.CreateDefaultBuilder(args)
-        //         .ConfigureWebHostDefaults(webBuilder =>
-        //         {
-        //             webBuilder.UseStartup<Startup>();
-        //         });
-
-
-
-//TODO how to have different keys between development-production. Does this key vault always get used.
- public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+        //TODO how to have different keys between development-production. Does this key vault always get used.
+        //Test to see if local user-secrets override the key-vault - we could create a unit test for this!
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                   .ConfigureAppConfiguration((context,config) =>
+                   .ConfigureAppConfiguration((context, config) =>
                     {
                         var builtConfig = config.Build();
 
@@ -41,16 +28,9 @@ namespace Garmin_To_Fitbit_Steps_Sync_Web
 
                         var keyuri = $"https://{builtConfig["KeyVaultName"]}.vault.azure.net/";
 
-                        config.AddAzureKeyVault(keyuri,keyVaultClient,new DefaultKeyVaultSecretManager());
+                        config.AddAzureKeyVault(keyuri, keyVaultClient, new DefaultKeyVaultSecretManager());
 
                     }).UseStartup<Startup>();
     }
-
-
-    
-
-
-
-
 
 }
