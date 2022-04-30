f039523e54d159b381b7a874c9e08525da10d60e
Matthew Eskolin <matteskolin@gmail.com>
2022-04-30 13:50:01 -0500

    progress on garmin web scraper

---
+++ GarminConnectScraper/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using PuppeteerSharp;
+
+namespace GarminConnectScraper // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //Log Yesterdays Steps
+            await GetYesterdaysSteps();
+        }
+
+        private static async Task GetYesterdaysSteps()
+        {
+            await new BrowserFetcher().DownloadAsync(BrowserFetcher.DefaultChromiumRevision);
+
+            Browser browser = await Puppeteer.LaunchAsync(new LaunchOptions() {Headless = true});
+
+            var page = await browser.NewPageAsync();
+
+            await page.GoToAsync("https://connect.garmin.com/signin/", WaitUntilNavigation.Networkidle0);
+
+            //gauth - widget - frame - gauth - widget'
+
+            var elementHandle = page.QuerySelectorAllAsync("#gauth-widget-frame-gauth-widget");
+
+        //var frame = await elementHandle.
+
+        //Resources for Next Time
+
+        //https://stackoverflow.com/questions/65049531/puppeteer-iframe-contentframe-returns-null
+        //C:\src\gh\garmin-connect-scraper\lib\main.js
+        //https://www.puppeteersharp.com/examples/index.html
+
+
+
+
+        }
+    }
+}
