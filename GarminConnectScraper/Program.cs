f4854fbc27952a610c1b309f02ea68ba6bdcb20c
Matthew Eskolin <matteskolin@gmail.com>
2022-05-23 21:38:22 -0500

    start cleanup

--- GarminConnectScraper/Program.cs
+++ GarminConnectScraper/Program.cs
@@ -23,64 +23,15 @@ namespace GarminConnectScraper
                 .AddJsonFile("appsettings.json")
                 .AddUserSecrets<Program>();
 
-
             //Load KeyVault
             var credential = new DefaultAzureCredential();
             config.AddAzureKeyVault(new System.Uri("https://kvfitbitapi.vault.azure.net/"), credential);
             var cnfg = config.Build();
 
-
-#if TEST
-
-            //Update Secrets
-
-            var apiTest = await FitBitAPI.InitializeApi(cnfg);
-
-            await apiTest.CreateDailySteps(DateTime.Parse("05/20/2022"), 1);
-
-
-            //new access_token
-
-            //string at = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMkJOVE0iLCJzdWIiOiI4TTNaOUgiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJ3YWN0IiwiZXhwIjoxNjUzMTc2MjQzLCJpYXQiOjE2NTMxNDc0NDN9.R9yswVZcCkHyHMuJeN1T0MAVVvvp_WDcU_kJ8Qbbp0Y";
-
-            ////new refresh token
-            //string rt = "e05447a33ccb2b56686650487ae49997f57998372d776f2934b3eff242c38256";
-
-
-                    //
-            //var r1 = await apiTest.UpdateKeyVault("Fitbit--AccessToken", at);
-
-            //Console.WriteLine();
-            //Console.WriteLine(r1.Value.Value);
-            //Console.WriteLine("Updated Access Token");
-
-            //var r2 = await apiTest.UpdateKeyVault("Fitbit--RefreshToken", rt);
-
-            //Console.WriteLine();
-            //Console.WriteLine(r2.Value.Value);
-            //Console.WriteLine("Updated Refresh Token");
-
-            //Update Azure Key Vault Refresh Token
-            //"Fitbit--RefreshToken"
-            return;
-#else
-
-
-            //var keyVaultUriString = $"https://{cnfg["KeyVaultName"]}.vault.azure.net/";
-            //var keyVaultUri = new System.Uri(keyVaultUriString);
-            //var client = new SecretClient(keyVaultUri, credential);
-
-            //var s1 = client.GetSecret("Fitbit--RefreshToken");
-            //Console.WriteLine("(1)" + s1.Value.Value);
-            //var s2 = client.GetSecret("Fitbit--AccessToken");
-            //Console.WriteLine("(2)" + s2.Value.Value);
-            //return;
-
             ////Run Main Program Tasks
             var program = new GarminConnectScraper(cnfg);
             await program.Run();
 
-#endif
 
 
         }
