ff7e9f2d6d0fdc14e8c5d1331f71ec0be5c48578
Matthew Eskolin <matteskolin@gmail.com>
2022-06-27 20:43:07 -0500

    cleanup

--- Garmin_To_Fitbit_Steps_Sync_Web/Program.cs
+++ Garmin_To_Fitbit_Steps_Sync_Web/Program.cs
@@ -1,14 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Azure.Identity;
-using Azure.Security.KeyVault.Secrets;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace Garmin_To_Fitbit_Steps_Sync_Web
 {
@@ -19,24 +12,10 @@ namespace Garmin_To_Fitbit_Steps_Sync_Web
             CreateWebHostBuilder(args).Build().Run();
         }
 
-
-
-
- public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+        private static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                   .ConfigureAppConfiguration((context,config) =>
+                   .ConfigureAppConfiguration((context, config) =>
                     {
-                        var builtConfig = config.Build();
-
-                        //Old Way
-                        //var azureServicetokenProvider = new AzureServiceTokenProvider();
-                        //var keyVaultClient = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(azureServicetokenProvider.KeyVaultTokenCallback));
-
-                        //var secretClient = new SecretClient(new Uri(builtConfig["KeyVaultName"]), new DefaultAzureCredential());
-
-                        //var keyuri = $"https://{builtConfig["KeyVaultName"]}.vault.azure.net/";
-
-                        //config.AddAzureKeyVault(keyuri,secretClient,new DefaultKeyVaultSecretManager());
 
                         var credential = new DefaultAzureCredential();
                         config.AddAzureKeyVault(new System.Uri("https://kvfitbitapi.vault.azure.net/"), credential);
@@ -46,7 +25,7 @@ namespace Garmin_To_Fitbit_Steps_Sync_Web
     }
 
 
-    
+
 
 
 
