2a8af2ad29f4d7173831381e6fb2112afb5ecbe8
Matthew Eskolin <matteskolin@gmail.com>
2022-12-13 21:19:35 -0600

    notes

--- GarminConnectScraper/RunBook/rb_StepScraper_Child.ps1
+++ GarminConnectScraper/RunBook/rb_StepScraper_Child.ps1
@@ -1,1 +1,6 @@
-﻿
+﻿https://stackoverflow.com/questions/56038716/can-we-deploy-azure-automation-account-and-runbook-through-visual-studio
+can we deploy a runbook through visual studio?
+
+"Staring GarminConnectScraper.exe..."
+Invoke-Command -ScriptBlock { &'C:\MyApps\GarminWebScraper\GarminConnectScraper.exe'} 
+"Done"
